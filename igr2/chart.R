9847e52b15b80460498b0700ddd1e85ac1cda41d
Victor Ordu <victorordu@outlook.com>
2019-01-07 17:23:13 +0100

    Adjusted chart annotations

--- igr2/chart.R
+++ igr2/chart.R
@@ -1,29 +1,29 @@
 # chart.R
 
-library(dplyr)
-library(rlang)
-library(stringr)
-library(ggplot2)
+library(tidyverse)
 
 draw_bar_chart <- function(data, para) {
-  options(scipen = 999)
-  
   if (!is.symbol(para))
     para <- as.symbol(para)
+  
   para <- enexpr(para)
-  xlabel <- para %>% 
-    deparse() %>% 
-    str_split('\\.') %>% 
-    unlist() %>% 
+  
+  xlabel <- para %>%
+    deparse %>%
+    str_split('\\.') %>%
+    unlist %>%
     str_c(collapse = ' ')
+  
   stub <- 'IGR disaggregated by'
-  data %>%
-    ggplot(aes(reorder(!!para, amt_m), y = amt_m, fill = !!para)) +
+  
+  gg <-
+    ggplot(data, aes(reorder(!!para, amt_m), y = amt_m, fill = !!para)) +
     geom_col() +
     coord_flip() +
     ggtitle(paste(stub, sQuote(xlabel))) +
     ylab('Amount (N million)') +
     xlab(xlabel) +
     theme(legend.position = 'none',
-          axis.text.x = element_text(deparse(para)))
+          axis.text.x = element_text())
+  print(gg)
 }
