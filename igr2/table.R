d95cc520a1e2238215e5a0381843b80ced3f8108
Victor Ordu <victorordu@outlook.com>
2019-01-07 17:18:55 +0100

    Modularised the code
    With additional code for statistical tables, reduced function complexity.

--- igr2/table.R
+++ igr2/table.R
@@ -1,26 +1,44 @@
 # table.R
 
 library(dplyr)
+library(magrittr)
 library(rlang)
 
 
-make_summ_table <- function(data, para) {
-  stopifnot(is.data.frame(data))
-  stopifnot(is.symbol(para))
-  
-  para <- enexpr(para)
-  data %>%
-    select(office:amount) %>%
-    group_by(!!para) %>%
-    summarise(
-      Total = sum(amount),
-      Mean = mean(amount, na.rm = TRUE),
-      Median = median(amount, na.rm = TRUE)
-    ) %>%
-    as.data.frame()
+
+make_summ_table <- function(data, param) {
+  .validateTabulationInputs(data, param)
+  .sumUpTheData(data, param) %>% 
+    arrange(desc(Total)) %>% 
+    mutate(Total = format(round(Total), big.mark = ','))
 }
 
 
-make_stat_table <- function(data) {
-  
+
+make_stat_table <- function(data, param) {
+  .validateTabulationInputs(data, param)
+  .sumUpTheData(data, param) %$%   # magrittr exposition operator
+  {
+    fivenum(Total)
+  }
+}
+
+
+## Aggregates the data along the lines of 'input' parameter
+## and returns the new data frame
+.sumUpTheData <- function(df = data.frame(), input = character())
+{
+  input <- if(!is.symbol(input))
+    as.symbol(input)
+  df %>%
+    group_by(!!input) %>%
+    summarise(Total = sum(amount))
+}
+
+
+
+.validateTabulationInputs <- function(dt, i)
+{
+  stopifnot(is.data.frame(dt))
+  stopifnot(is.character(i))
 }
